6f423e61a08de25b25a570d7126dbed25993c983
pid123or123as <banderovbahmutov@gmail.com>
2025-11-01 21:06:34 +0300

    Update stupid.lua

--- stupid.lua
+++ stupid.lua
@@ -1,10 +1,17 @@
-print('3')
 local Sperma = {}
+
+-- Ждём UI, но НЕ внутри функции Init
 task.defer(function()
-    repeat
-        task.wait()
-    until UI and UI.Tabs and UI.Tabs.AutoFarm
+    repeat task.wait() until UI and UI.Tabs and UI.Tabs.AutoFarm
+    Sperma:Init(UI, Core, notify)  -- manual idk for what
+end)
+
 function Sperma.Init(UI, Core, notify)
+    if not (UI and UI.Sections and UI.Sections.Gunmods) then
+        warn("stupid")
+        return
+    end
+
     UI.Sections.Gunmods:Header({ Name = 'Gun Modification' })
 
     UI.Sections.Gunmods:Toggle({
@@ -18,32 +25,30 @@ function Sperma.Init(UI, Core, notify)
 
                 local function applyGunMods()
                     local success, GunModule = pcall(function()
-                        return require(
-                            ReplicatedStorage.Modules.Game.ItemTypes.Gun
-                        )
+                        return require(ReplicatedStorage.Modules.Game.ItemTypes.Gun)
                     end)
 
-                    if success and GunModule then
-                        local originalApplyRecoil = GunModule.apply_recoil
-                        GunModule.apply_recoil = function(gun)
-                            return
+                    if not success or not GunModule then return end
+
+                    -- Отключаем отдачу
+                    local originalApplyRecoil = GunModule.apply_recoil
+                    GunModule.apply_recoil = function() end
+
+                    -- Хукаем класс
+                    GunModule.class.hook(function(gunObj)
+                        gunObj.states.fire_rate.set(3000)
+                        gunObj.states.accuracy.set(100)
+                        gunObj.states.reload_time.set(0.1)
+                        gunObj.states.last_shot.set(-999999)
+                        gunObj.instance:SetAttribute('Recoil', 0)
+
+                        local oldGet = gunObj.states.last_shot.get
+                        gunObj.states.last_shot.get = function()
+                            return 0
                         end
 
-                        GunModule.class.hook(function(gunObj)
-                            gunObj.states.fire_rate.set(3000)
-                            gunObj.states.accuracy.set(100)
-                            gunObj.states.reload_time.set(0.1)
-                            gunObj.states.last_shot.set(-999999)
-                            gunObj.instance:SetAttribute('Recoil', 0)
-
-                            local originalLastShot = gunObj.states.last_shot.get
-                            gunObj.states.last_shot.get = function()
-                                return 0
-                            end
-
-                            return function() end
-                        end)
-                    end
+                        return function() end
+                    end)
                 end
 
                 applyGunMods()
@@ -54,11 +59,7 @@ function Sperma.Init(UI, Core, notify)
                     applyGunMods()
                 end)
 
-                local heartbeatConnection
-                heartbeatConnection = RunService.Heartbeat:Connect(function()
-                    applyGunMods()
-                end)
-
+                local heartbeatConnection = RunService.Heartbeat:Connect(applyGunMods)
                 _G.GunModsHeartbeat = heartbeatConnection
             else
                 if _G.GunModsHeartbeat then
@@ -69,8 +70,5 @@ function Sperma.Init(UI, Core, notify)
         end,
     })
 end
-end)
+
 return Sperma
-
-
-
